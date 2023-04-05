eff2168d393f70f32484fcc25dbab94c79d55261
maej <muhammadsesay8@gmail.com>
2023-04-05 08:45:27 +0000

    Add: next image to fix gif asset loading

--- frontend/components/landingBox.tsx
+++ frontend/components/landingBox.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
@@ -21,7 +22,14 @@ const LandingBox = () => {
     >
       {/* image and button section */}
       <div className="flex flex-col items-center justify-center mb-10">
-        <div className="bg-logo bg-cover bg-center w-40 h-40 rounded-full mb-5" />
+        <div className="rounded-full bg-white w-40 h-40 flex items-center justify-center mb-5 overflow-hidden">
+          <Image
+            src="/images/logo.gif"
+            alt="bird-logo"
+            width={400}
+            height={400}
+          />
+        </div>
         <button
           className="bg-blue-500 p-2 rounded-full w-20 shadow-md hover:bg-blue-600 text-white"
           type="submit"

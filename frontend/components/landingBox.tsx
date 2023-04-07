60194b9d9eed12d6b8261f977d1a1ef76af62a9e
maej <muhammadsesay8@gmail.com>
2023-04-07 23:21:53 +0000

    Add: responseive for landingBox

--- frontend/components/landingBox.tsx
+++ frontend/components/landingBox.tsx
@@ -36,16 +36,16 @@ const LandingBox = () => {
     }
 
     return (
-        <section className="flex flex-col items-center justify-center">
+        <section className="flex flex-col items-center justify-center -mt-28 md:-mt-auto">
             <h2
-                className={`${press_start_2P.className} w-1/2 text-center text-xs text-gray-600 leading-loose`}
+                className={`${press_start_2P.className} mx-10 md:mx-0 md:w-1/2 text-center text-sm md:text-xl text-gray-600 leading-6 md:leading-loose`}
             >
                 The Web3 Marketing Platform - Empowering Threadors and Automating Marketing
                 Campaigns for Web3 Protocols
             </h2>
             <form
                 onSubmit={handleSubmit(onSubmit)}
-                className="flex flex-col items-center justify-start w-1/4 h-1/2"
+                className="flex flex-col items-center justify-start w-2/5 h-1/2"
             >
                 {/* image and button section */}
                 <div className="flex flex-col items-center justify-center mb-3">
@@ -61,15 +61,17 @@ const LandingBox = () => {
                 </div>
 
                 {/* instruction text and input field section */}
-                <div className="my-5 space-y-3">
-                    <p className={`${press_start_2P.className} text-center text-xs text-gray-600`}>
+                <div className="my-10 space-y-3">
+                    <p
+                        className={`${press_start_2P.className} text-center text-gray-600 text-xs md:text-sm leading-7`}
+                    >
                         Provide Email For Private Beta
                     </p>
                     <div className="h_line"></div>
                     <input
                         type="email"
                         placeholder="youremail@example.com"
-                        className={`${press_start_2P.className} text-xs w-full h-8 rounded-sm p-4 text-gray-500 border-2 border-gray-400`}
+                        className={`${press_start_2P.className} text-xs w-full h-14 md:h-12 rounded-sm p-4 text-gray-500 border-2 border-gray-400`}
                         {...register("email", { required: "This field is required" })}
                     />
 
@@ -78,7 +80,7 @@ const LandingBox = () => {
                     )}
                 </div>
                 <button
-                    className={`${press_start_2P.className} flex justify-center text-xs bg-blue-500 p-2 rounded-full w-24 shadow-md hover:bg-blue-600 text-white`}
+                    className={`${press_start_2P.className} flex items-center justify-center text-sm bg-blue-500 p-2 rounded-full w-40 h-14 shadow-md hover:bg-blue-600 text-white`}
                     type="submit"
                 >
                     {requesting ? <Spinner width={20} height={20} /> : "Submit"}

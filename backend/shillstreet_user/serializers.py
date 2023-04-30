9c21555a2bba46daf39e29f1fa5b0a69624b3ffb
wg326 <57165451+wg326@users.noreply.github.com>
2023-04-30 11:35:16 -0700

    update

--- backend/shillstreet_user/serializers.py
+++ backend/shillstreet_user/serializers.py
@@ -8,17 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'email', 'walletAddress',
                   'privateString', 'twitter_handle']
         extra_kwargs = {
-            'privateString': {'write_only': True},
             'twitter_handle': {'required': False}
         }
-
-    def save(self, **kwargs):
-        private_string = self.validated_data.pop('privateString', None)
-        user = super().save(**kwargs)
-        if private_string is not None:
-            user.set_password(private_string)
-            user.save()
-        return user
 
 
 class WaitListSerializer(serializers.ModelSerializer):

499815b6079b58e2a4c9b24316fa076e86a70ad6
wg326 <57165451+wg326@users.noreply.github.com>
2023-04-30 09:56:23 -0700

    update

--- backend/shillstreet_user/urls.py
+++ backend/shillstreet_user/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import RegisterView, LoginView, UserView, LogoutView, BindTwitterView, UnbindTwitterView, WaitList
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view()),
-    path('bind', BindTwitterView.as_view()),
-    path('unbind_twitter', UnbindTwitterView.as_view()),
-    path('join_waitlist', WaitList.as_view())
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('user/', UserView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('bind/', BindTwitterView.as_view()),
+    path('unbind_twitter/', UnbindTwitterView.as_view()),
+    path('join_waitlist/', WaitList.as_view())
 
 ]

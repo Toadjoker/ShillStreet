d21de79f2bbce5bf6b1f3f30fee0cbcb7b48e5e6
wg326 <57165451+wg326@users.noreply.github.com>
2023-04-30 20:49:10 -0700

    update

--- backend/shillstreet_user/readTweet.py
+++ backend/shillstreet_user/readTweet.py
@@ -9,6 +9,8 @@ def readTweet(url):
     chromedriver_autoinstaller.install()
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     tweet_text = WebDriverWait(driver, 5).until(

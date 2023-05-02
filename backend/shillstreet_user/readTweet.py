e39e85072a9936e08bf1a739f2761058c0e70d43
wg326 <57165451+wg326@users.noreply.github.com>
2023-05-02 14:28:01 -0700

    update

--- backend/shillstreet_user/readTweet.py
+++ backend/shillstreet_user/readTweet.py
@@ -14,7 +14,11 @@ def readTweet(url):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     tweet_text = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[2]/div/div/span"))).text
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[2]/div/div/span[2]"))).text
     current_url = driver.current_url
     driver.quit()
+    print(tweet_text)
     return(tweet_text, current_url.split("/")[3])
+
+
+readTweet("https://twitter.com/oMysoftwai/status/1653501699905015809")
